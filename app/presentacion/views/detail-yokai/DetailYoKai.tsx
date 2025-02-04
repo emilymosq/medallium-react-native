57b541059b91087afede8ba55f86e77195765a3c
emilymosq <emilymosq4@gmail.com>
2025-02-04 23:52:27 +0100

    Empezando la vista de detalle yo-kai

---
+++ app/presentacion/views/detail-yokai/DetailYoKai.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Image, Text } from "react-native";
+import { styles } from "./StylesDetailYoKai";
+
+const DetailYoKai = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.topSection}>
+                <Image source={require("../../../../assets/back.png")} style={styles.icon} />
+                <Image source={require("../../../../assets/heartw.png")} style={styles.icon} />
+            </View>
+            <View style={styles.yoKaiContainer}>
+                <View style={styles.yoKai}>
+                    <Text style={styles.yoKaiName}>Jibanyan</Text>
+                    <Text style={styles.yoKaiName}>ジバニャン</Text>
+                    <Image source={require("../../../../assets/jibanyan2.png")} style={styles.yoKaiImage} />
+                </View>
+                <View style={styles.tribu}>
+                    <Image source={require("../../../../assets/guapo.png")} style={styles.tribuImagen} />
+                </View>
+            </View>
+            <View style={styles.bottomSection}>
+                <Image source={require("../../../../assets/addyokai.png")} style={styles.iconAdd} />
+                <View style={styles.extra}>
+                    <Text>Fuego</Text>
+                    <Image source={require("../../../../assets/fuego.png")} style={styles.icon} />
+                </View>
+                <View style={styles.extra}>
+                    <Text>Rango</Text>
+                    <Image source={require("../../../../assets/rangod.png")} style={styles.icon} />
+                </View>
+            </View>
+        </View>
+    );
+};
+
+export default DetailYoKai;
---
+++ app/presentacion/views/detail-yokai/StylesDetailYoKai.tsx
@@ -0,0 +1,76 @@
+import { StyleSheet } from "react-native";
+import { AppColors } from "../../themes/AppTheme";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: AppColors.background,
+        alignItems: "center",
+    },
+    topSection: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "80%",
+        marginTop: 45,
+    },
+    icon: {
+        width: 23,
+        height: 23,
+    },
+    iconAdd: {
+        width: 38,
+        height: 38,
+    },
+    yoKaiContainer: {
+        alignItems: "center",
+        width: "100%",
+    },
+    yoKai: {
+        width: 280,
+        height: 280,
+        backgroundColor: AppColors.tribuGuapo,
+        borderRadius: 200,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    yoKaiName: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    yoKaiImage: {
+        width: 160,
+        height: 160,
+        resizeMode: "contain",
+        marginVertical: 5,
+    },
+    tribu:{
+        width: "100%",
+        justifyContent:"center",
+        alignItems: "center",
+        bottom: 5
+    },
+    tribuImagen: {
+        width: 55,
+        height: 55,
+        resizeMode: "contain",
+        position: "absolute"
+    },
+    bottomSection: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        marginVertical: 35,
+        gap: 5,
+    },
+    extra: {
+        flexDirection: "row",
+        alignItems: "center",
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        borderColor: "#535353",
+        borderWidth: 1,
+        borderRadius: 50,
+        gap: 8,
+    },
+});
